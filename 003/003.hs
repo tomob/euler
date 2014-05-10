0249a258a446e9000f6635b7db181a00421ca915
Tomo <yubizume@gmail.com>
2014-05-11 00:02:36 +0200

    First Haskell version

---
+++ 003/003.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.Numbers.Primes
+
+factors :: Integer -> [Integer]
+factors n = factors' [] n primes
+    where
+      factors' fs 1 _      = fs
+      factors' fs n (p:ps)
+          | n `mod` p == 0 = factors' (p:fs) (n `div` p) ps
+          | otherwise      = factors' fs n ps
+
+candidatePrimes :: Integer -> [Integer]
+candidatePrimes n = reverse . takeWhile (<= roundedRoot) $ primes
+    where
+      roundedRoot = toInteger $ floor $ sqrt $ fromInteger n
+
+largestPrime :: Integer -> Integer
+largestPrime n = largestPrime' $ candidatePrimes n
+    where
+      largestPrime' (x:xs)
+          | n `rem` x == 0 = x
+          | otherwise      = largestPrime' xs
+
+main = do
+  putStrLn $ "Largest: " ++ show (largestPrime 600851475143)
---
+++ 003/README.md
@@ -0,0 +1,4 @@
+Project Euler -- Problem 003
+============================
+
+This is a stub.
