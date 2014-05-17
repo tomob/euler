2d9fe46bc8d93e3e858ca49acc68a5c085edaf83
tomob <yubizume@gmail.com>
2014-05-17 21:38:13 +0200

    Problem 007 - C++

--- 007/007.cc
+++ 007/007.cc
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <climits>
+#include <cmath>
 
 using namespace std;
 
+bool isPrime(unsigned number)
+{
+    if (number <=1)
+        return false;
+    if (number == 2)         
+        return true;
+    if (number % 2 == 0)
+        return false;
+
+    bool prime = true;
+    int divisor = 3;
+    unsigned upperLimit = static_cast<unsigned>(sqrt(number) + 1);
+        
+    while (divisor <= upperLimit) {
+        if (number % divisor == 0)
+            prime = false;
+        divisor +=2;
+    }
+
+    return prime;
+}
+
 int main()
 {
-    cout << "Not implemented yet" << endl;
+    unsigned long counter = 0;
+
+    for (unsigned long i=2; i<ULONG_MAX; i++) {
+        if (isPrime(i)) counter++;
+        if (counter == 10001) {
+            cout << "10001th prime: " << i << endl;
+            break;
+        }
+    }
+    
     return 0;
 }
