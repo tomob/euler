8250b40cc98ef33cbdd9bc5b01f2e4313011beb5
Tomo <yubizume@gmail.com>
2014-05-29 23:50:35 +0200

    Problem 009 - Haskell

--- 009/009.hs
+++ 009/009.hs
@@ -1,4 +1,17 @@
 module Main where
 
+triplets :: [(Int, Int, Int)]
+triplets = [(x, y, 1000-x-y) | x <- [1..1000], y <- [x..1000-x]]
+
+pythagoean :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+pythagoean trs = filter isPitagorean trs
+    where
+      isPitagorean (a, b, c) = a*a + b*b == c*c
+
+multiply :: [(Int, Int, Int)] -> [Int]
+multiply ps = map multiply' ps
+    where
+      multiply' (a, b, c) = a*b*c
+           
 main = do
-  putStrLn "Not implemented yet"
+  putStrLn $ "a*b*c: " ++ show (head ((multiply . pythagoean) triplets))
